7db3dae4b82c202d6f58fbc48a8c49a5a9dee475
Yassin Elsawy <yassinsawy@outlook.com>
2025-10-26 00:37:25 +0300

    added Sequential Search

--- Search Program.cpp
+++ Search Program.cpp
@@ -2,6 +2,32 @@
 #define ll long long
 using namespace std;
 
+class Sequential_Search{
+    int n;
+    vector<int> v;
+public:
+    void take_input(){
+        v.resize(n);
+        cout << "Enter the number of elements: ";
+        cin >> n;
+        cout << "Enter the elements: ";
+        for (int i = 0; i < n; ++i) {
+            cin >> v[i];
+        }
+    }
+
+    void seqSearch(ll x){
+        for (int i = 0; i < v.size(); ++i) {
+            if(v[i] == x){
+                cout << "The element " << x << " is found at index " << i << endl;
+                return;
+            }
+        }
+        cout << "The element " << x << " is not found in the array." << endl;
+    }
+};
+
+
 int main() {
     std::cout << "Hello from search!" << std::endl;
     return 0;
