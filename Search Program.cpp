92caaf59ddbd0cd3d250237ef22799adc0079ec9
Abdelrahman <abdelrahmanyr05@gmail.com>
2025-10-26 20:10:41 +0300

    Create menu

--- Search Program.cpp
+++ Search Program.cpp
@@ -70,16 +70,82 @@ public:
 };
 
 
-int main() {
-    cout << "Hello from search!" << std::endl;
-    int n = 5;
-    int arr[] = {1, 3, 5, 6, 7};
-    SequentialSearch<int> seq;
-    printSearchResult(arr, seq.iterative(arr, n, 6));
-    printSearchResult(arr, seq.recursive(arr, n, 8, 0));
+//int main() {
+//    cout << "Hello from search!" << std::endl;
+//    int n = 5;
+//    int arr[] = {1, 3, 5, 6, 7};
+//    SequentialSearch<int> seq;
+//    printSearchResult(arr, seq.iterative(arr, n, 6));
+//    printSearchResult(arr, seq.recursive(arr, n, 8, 0));
+//
+//    BinarySearch<int> bin;
+//    printSearchResult(arr, bin.iterative(arr, n, 4));
+//    printSearchResult(arr, bin.recursive(arr, n, 7, 0, n - 1));
+//
+//
+//    return 0;
+//}
 
-    BinarySearch<int> bin;
-    printSearchResult(arr, bin.iterative(arr, n, 4));
-    printSearchResult(arr, bin.recursive(arr, n, 7, 0, n - 1));
+int main() {
+    cout << "Welcome to the Search Program!" << endl;
+    char again;
+
+    do {
+        cout << "\nChoose a search method:\n";
+        cout << "1) Sequential Search (Iterative)\n";
+        cout << "2) Sequential Search (Recursive)\n";
+        cout << "3) Binary Search (Iterative)\n";
+        cout << "4) Binary Search (Recursive)\n";
+        cout << "Enter your choice (1-4): ";
+
+        int choice;
+        cin >> choice;
+
+        cout << "Enter the size of the array: ";
+        int n;
+        cin >> n;
+
+        int arr[n];
+        cout << "Enter " << n << " elements (sorted for binary search): ";
+        for (int i = 0; i < n; ++i) {
+            cin >> arr[i];
+        }
+
+        cout << "Enter the value to search for: ";
+        int val;
+        cin >> val;
+
+        SequentialSearch<int> seq;
+        BinarySearch<int> bin;
+
+        cout << "\nResult: ";
+        int res = -1;
+        switch (choice) {
+            case 1:
+                res = seq.iterative(arr, n, val);
+                break;
+            case 2:
+                res = seq.recursive(arr, n, val, 0);
+                break;
+            case 3:
+                res = bin.iterative(arr, n, val);
+                break;
+            case 4:
+                res = bin.recursive(arr, n, val, 0, n - 1);
+                break;
+            default:
+                cout << "Invalid choice! Please select from the list.\n";
+        }
+
+        printSearchResult(arr, res);
+
+        cout << "\nWould you like to perform another search? (y/n): ";
+        cin >> again;
+
+    } while (again == 'y' || again == 'Y');
+
+    cout << "\nThank you for using the Search Program!\n";
+    cout << "Goodbye!\n";
+
     return 0;
 }
